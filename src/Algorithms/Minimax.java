61ed3ad230617dc86e436ae6b8775517f51e01b9
NicholasLiem <13521135@std.stei.itb.ac.id>
2023-10-14 22:23:36 +0700

    docs: adding comment

--- src/Algorithms/Minimax.java
+++ src/Algorithms/Minimax.java
@@ -5,14 +5,20 @@ import Algorithms.DataStructure.TreeNode;
 
 public class Minimax {
     public TreeNode<Double> minimax(TreeNode<Double> node, double alpha, double beta, boolean isMaximizingPlayer) {
+        // Return if terminal node
         if (node.isLeaf()) {
             return node;
         }
 
+        // Init best move and best score
         TreeNode<Double> bestMove = null;
         double bestScore;
 
         if (isMaximizingPlayer) {
+            // Kalau maximizing then bestScorenya set -Inf, iterate for each childrennya dan sampai terminal node,
+            // Kalau udah diterminal node nanti kan return nilainya di cek apakah nilainya lebih besar dari bestScore (kasusnya Maximizing)
+            // If yes, then update bestScore dan set bestMove to it, also update the current alpha
+            // Alpha --> Maximizing antara nilai sekarang dengan alpha, kalo nilai Alpha >= beta dipotong cabangnya.
             bestScore = Double.NEGATIVE_INFINITY;
             for (TreeNode<Double> child : node.getChildren()) {
                 TreeNode<Double> nextMove = minimax(child, alpha, beta, false);
@@ -22,11 +28,12 @@ public class Minimax {
                     bestMove = child;
                 }
                 alpha = Math.max(alpha, bestScore);
-                if (beta <= alpha) {
-                    break;  //
+                if (alpha >= beta) {
+                    break;
                 }
             }
         } else {
+            // Penjelasannya sama kek sebelumnya tapi kebalikan aja, tapi untuk pemotongan cabang ttp sama.
             bestScore = Double.POSITIVE_INFINITY;
             for (TreeNode<Double> child : node.getChildren()) {
                 TreeNode<Double> nextMove = minimax(child, alpha, beta, true);
@@ -36,7 +43,7 @@ public class Minimax {
                     bestMove = child;
                 }
                 beta = Math.min(beta, bestScore);
-                if (beta <= alpha) {
+                if (alpha >= beta) {
                     break;
                 }
             }
