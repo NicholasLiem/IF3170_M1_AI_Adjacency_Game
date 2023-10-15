c45af41fc3e4ca3975b4d6159f16b3084e24e0bc
febryanarota <arota0202@gmail.com>
2023-10-15 21:34:22 +0700

    change heuristic

---
+++ src/Algorithms/LocalAgent.java
@@ -0,0 +1,10 @@
+package Algorithms;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class LocalAgent {
+    public int[] move (int[][] board, int roundsLeft) {
+        return null;
+    }
+}
